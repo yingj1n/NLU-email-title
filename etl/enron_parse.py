d0a2ce451d55caf2a3f1bf7b621d331b123b04d1
Ying Jin <yingjin1026@gmail.com>
2019-04-14 22:53:57 -0400

    removed unnecessary files

--- etl/enron_parse.py
+++ etl/enron_parse.py
@@ -1,5 +1,18 @@
 import re
 import pandas as pd
+from tqdm import tqdm
+
+def parse_all(docs):
+    allkeys = find_allkeys(docs)
+    parsed = list()
+    for doc in tqdm(docs):
+        d = parse_doc(doc, allkeys)
+        body = d['Body']
+        if body.find('Forwarded by') == -1 and body.find('Original Message') == -1 and body.find('Subject:') == -1:
+            body_list = map(str.strip, body.splitlines())
+            d['Body'] = ' '.join(body_list) #list(filter(None, body_list))
+            parsed.append(d)
+    return parsed
 
 def parse_doc(doc, allkeys):
 
